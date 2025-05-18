708576838e56cc9c9082d0192cd21b6d2bef3992
KrrishBhandari <bhandarikrishna057@gmail.com>
2025-05-18 07:26:11 +0530

    added Dashboard page and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage";
 
 const queryClient = new QueryClient();
 
@@ -16,9 +17,10 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/create-agent" element={<Index />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/create-agent" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
